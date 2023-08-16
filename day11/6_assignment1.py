3d94204b68627d25ac324a5eae88590a64740370
Deepikasainju <dpisaiyo@gmail.com>
2023-08-16 16:45:30 +0545

    assignment

--- day11/6_assignment1.py
+++ day11/6_assignment1.py
@@ -3,6 +3,7 @@ n=int(input("enter an integer:"))
 sum=0
 for i in range(1,6):
     sum+=n**i
+    print(sum)
 print(sum)
 
 # wap to calculate difference between a given number and 17. if the number is greater 
@@ -21,11 +22,11 @@ count=0
 if n==0 or n==1:
     print(f"{n} is not a prime number.")
 else:
-    for i in range(2,n):
+    for i in range(2,n+1):
         if n%i==0:
             count+=1
 
-    if count>2:
+    if count>1:
         print(f"{n} is not a prime number.")
     else:
         print(f"{n} is a prime number.")
