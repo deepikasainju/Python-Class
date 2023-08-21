cf88b22b3ae38fd9ff1bc9256fd13b864bc8e988
Deepikasainju <dpisaiyo@gmail.com>
2023-08-21 17:56:31 +0545

    day15

---
+++ day15/task1.py
@@ -0,0 +1,13 @@
+# create a class Person with instance attributes name and age.
+# create a method get_details in this class to print name and age
+
+class Person:
+    def __init__(self, name, age):
+        self.name=name
+        self.age=age
+    
+    def get_details(self):
+        return f"the name is {self.name} and age is {self.age}"
+    
+p=Person("shyam",23)
+print(p.get_details())
